3ddc86a41c1947a2568f89f2a8781613b5be9861
Blank <Blank@DESKTOP-759FKM4>
2020-07-18 16:11:25 +0500

    movementupdatedand set

--- Boast-game/Assets/Rocket.cs
+++ Boast-game/Assets/Rocket.cs
@@ -5,31 +5,76 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcs=100f;
+    [SerializeField] float mainthurst = 100f;
+    Rigidbody mybody;
+    AudioSource audiosource;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mybody = GetComponent<Rigidbody>();
+        audiosource = GetComponent<AudioSource>();
+        audiosource.Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thurst();
+        Rotate();
     }
 
-    private void ProcessInput()
+    void Thurst()
     {
-        if(Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
-            print("Space Pressed");
+            mybody.AddRelativeForce(Vector3.up*mainthurst);
+            if (!audiosource.isPlaying)
+            {
+                audiosource.Play();
+            }
         }
+        else
+        {
+            audiosource.Stop();
+        }
+    }
+    private void Rotate()
+    {
+        mybody.freezeRotation = true;
         if (Input.GetKey(KeyCode.A))
         {
-            print("Left Pressed");
+            float rotatethisframe = rcs * Time.deltaTime;
+            transform.Rotate(Vector3.forward*rotatethisframe);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            print("Right Pressed");
+            float rotatethisframe = rcs * Time.deltaTime;
+            transform.Rotate(Vector3.back * rotatethisframe);
+          
+           
         }
+        mybody.freezeRotation = false;
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                //do nothing
+                break;
+            case "Fuel":
+                //refuel
+                break;
+            default:
+                //die
+                break;
+
+        }
+
     }
 }
+
+
